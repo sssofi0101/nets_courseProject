4706ffd2a2e0e36eb3e24cbb09188ec7151f1f1b
sssofi0101 <sssofi0101@gmail.com>
2021-11-20 21:11:50 +0300

    Добавила классы сетей

---
+++ nets/HopfieldNet.py
@@ -0,0 +1,12 @@
+class HopfieldNet:
+    def __init__(self, S_entries_layer = None, R_layer = None, S_outputs_layer = None):
+        """Constructor"""
+        self.S_entries_layer = S_entries_layer
+        self.R_layer = R_layer
+        self.S_outputs_layer = S_outputs_layer
+
+    def train(self, list_of_paths, reference_count):
+        pass
+
+    def getResult(self, image_data):
+        pass
---
+++ nets/Perceptron.py
@@ -0,0 +1,12 @@
+class Perceptron:
+    def __init__(self, S_layer = None,  A_layer = None, R_layer = None):
+        """Constructor"""
+        self.S_layer = S_layer
+        self.A_layer = A_layer
+        self.R_layer = R_layer
+
+    def train(self, list_of_paths, classes_count):
+        pass
+
+    def getResult(self, image_data):
+        pass
