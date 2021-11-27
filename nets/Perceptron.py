37c8dbe40bcce3ff0068b598d856c33e0a594faf
sssofi0101 <sssofi0101@gmail.com>
2021-11-27 23:09:56 +0300

    Продолжила писать train в перцептроне

--- nets/Perceptron.py
+++ nets/Perceptron.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 from A import A
 from R import R
 from S import S
@@ -27,11 +30,47 @@ class Perceptron:
         height = im.size[1]
         s_count = width*height
         for i in range(s_count):
-            self.S_layer.append(A(None, None, None))
+            self.S_layer.append(S(None, None, None))
         for i in range(len(list_of_paths)):
             self.A_layer.append(A(self.S_layer, None, None))
         for i in range(len(classes_count)):
-            self.R_layer.append(A(self.A_layer, None, None))
+            self.R_layer.append(R(self.A_layer, None, None))
+
+        def logistic(x):
+            return 1.0 / (1 + math.exp(-x))
+
+        def logistic_proizvodn(x):
+            return logistic(x) * (1 - logistic(x))
+
+        l_speed = 1
+        epoch_count = 5
+
+        random.seed(1)
+        for a in self.A_layer:
+            for i in range(s_count):
+                a.weights.append(random.uniform(-1, 1))
+        for r in self.R_layer:
+            for i in range(len(list_of_paths)):
+                r.weights.append(random.uniform(-1, 1))
+
+        training_data =[]
+        for i in range(len(list_of_paths)):
+            img = Image.open(list_of_paths[i])
+            pix = img.load()
+            s1=[]
+            for w in range(width):
+                for h in range(height):
+                    if pix[w, h]==(0, 0, 0):
+                        s1.append(1)
+                    elif pix[w, h]==(255, 255, 255):
+                        s1.append(0)
+            training_data.append(s1)
+        target_output = []
+
+
+
+
+
 
 
     def getResult(self, image_data):
