b1556373ed287aeb18880914d1c360213b83d716
sssofi0101 <sssofi0101@gmail.com>
2021-11-27 17:04:05 +0300

    Изменила конструктор Перцептрона, начала писать функцию train

--- nets/Perceptron.py
+++ nets/Perceptron.py
@@ -1,12 +1,38 @@
+from A import A
+from R import R
+from S import S
+from PIL import Image
+
+
 class Perceptron:
-    def __init__(self, S_layer = None,  A_layer = None, R_layer = None):
+    """S_layer - число S нейронов,  A_layer - число A-нейронов, R_layer - число R-нейронов"""
+    def __init__(self, S_layer=None,  A_layer=None, R_layer=None):
         """Constructor"""
-        self.S_layer = S_layer
-        self.A_layer = A_layer
-        self.R_layer = R_layer
+        self.S_layer = []
+        self.A_layer = []
+        self.R_layer = []
+        if S_layer is not None:
+            for i in range(S_layer):
+                self.S_layer.append(S(None, None, None))
+        if A_layer is not None:
+            for i in range(A_layer):
+                self.A_layer.append(A(self.S_layer, None, None))
+        if R_layer is not None:
+            for i in range(R_layer):
+                self.R_layer.append(R(self.A_layer, None, None))
 
     def train(self, list_of_paths, classes_count):
-        pass
+        im = Image.open(list_of_paths[0])
+        width = im.size[0]
+        height = im.size[1]
+        s_count = width*height
+        for i in range(s_count):
+            self.S_layer.append(A(None, None, None))
+        for i in range(len(list_of_paths)):
+            self.A_layer.append(A(self.S_layer, None, None))
+        for i in range(len(classes_count)):
+            self.R_layer.append(A(self.A_layer, None, None))
+
 
     def getResult(self, image_data):
         pass
