1700a32a52b5c1680fa193f3a32ec51a31ddcd3e
sssofi0101 <sssofi0101@gmail.com>
2021-12-24 13:16:42 +0300

    Добавила возможность создавать перцептрон с указанием количества нейронов. Добавила документацию.

--- nets/Perceptron.py
+++ nets/Perceptron.py
@@ -24,36 +24,16 @@ class Perceptron:
         for i in range(R_layer_count):
             self.R_layer.append(0)
             self.R_layer[i] = R(self.A_layer)
-        """for r in range(len(self.R_layer)):
-            #for j in range(len(self.A_layer)):
-            j=18
-            while j>0:
-                 self.R_layer[r].weights.append(0)
-                 j-=1
-        #random.seed(1)
-        for a in range(len(self.A_layer)):
-            for i in range(len(self.S_layer)):
-                self.A_layer[a].weights.append(random.randint(-1, 1))"""
 
-
-    def train(self, list_of_paths):  # здесь тогда передавать target_output и training_data
-        # перенести потом в главную программу до вызова конструктора. отсюда
+    def train(self, list_of_paths):
 
         im = Image.open(list_of_paths[0])
         width = im.size[0]
         height = im.size[1]
-        s_count = width*height  # досюда
+        s_count = width*height
 
-        """def logistic(x):
-            return 1.0 / (1 + math.exp(-x))
-
-        def logistic_proizvodn(x):
-            return logistic(x) * (1 - logistic(x))
-
-        l_speed = 1"""
         epoch_count = 50
 
-        # передавать в этот метод уже training_data, а то что ниже перенести в основную программу
         training_data = []
         for i in range(len(list_of_paths)):
             img = Image.open(list_of_paths[i])
@@ -70,28 +50,28 @@ class Perceptron:
 
         target_output = []
         for i in range(len(list_of_paths)):
-            path = os.path.dirname(list_of_paths[i]) # нужно ли мне здесь dirname, если это итак список путей?..
+            path = os.path.dirname(list_of_paths[i])
             folder = os.path.basename(path)
             target_output.append(folder)
-        training_count = len(list_of_paths)  # досюда
+        training_count = len(list_of_paths)
 
         for epoch in range(epoch_count):
-            for sample in range(training_count):  #отсюда
+            for sample in range(training_count):
                 for i in range(len(self.S_layer)):
                     self.S_layer[i].current_value = training_data[sample][i]
                 for i in range(len(self.A_layer)):
                     self.A_layer[i].getValue()
                 for i in range(len(self.R_layer)):
-                    self.R_layer[i].getValue()  # досюда можно перенести в get_result и вызывать уже его
+                    self.R_layer[i].getValue()
                 for i in range(len(self.R_layer)):
-                    if i == int(target_output[sample]):  # номер р-нейрона соотв классу кот он определяет напр 0 нейрон определяет цифру 0
+                    # номер р-нейрона соотв классу кот он определяет напр 0 нейрон определяет цифру 0
+                    if i == int(target_output[sample]):
                         # проверяем знак ошибки целевого r-нейрона
                         if self.R_layer[int(target_output[sample])].current_value - 1 > 0:  #  -int(target_output[sample])
                             for j in range(self.R_layer[int(target_output[sample])].entries):
                                 if self.R_layer[int(target_output[sample])].entries[j].current_value == 1:
                                     self.R_layer[int(target_output[sample])].weights[j] -= 1
                         elif self.R_layer[int(target_output[sample])].current_value - 1 < 0:
-                        #if self.R_layer[int(target_output[sample])].current_value - 1 < 0:
                             for j in range(len(self.R_layer[int(target_output[sample])].entries)):
                                 if self.R_layer[int(target_output[sample])].entries[j].current_value == 1:
                                     self.R_layer[int(target_output[sample])].weights[j] += 1
@@ -104,17 +84,8 @@ class Perceptron:
                             for j in range(len(self.R_layer[i].entries)):
                                 if self.R_layer[i].entries[j].current_value == 1:
                                     self.R_layer[i].weights[j] += 1
-        """
-        #прямое распространение ошибки
-        for epoch in range(epoch_count):
-            for sample in range(training_count):
-                for node in range(self.A_layer):
-                    self.A_layer[node].current_value = logistic(self.A_layer[node].getValue())
-                for r in range(self.R_layer):
-                    self.R_layer[r].current_value=logistic(self.R_layer[r].getValue())
-                # постактивация - значение, которое должно получится
-                FE = self.R_layer[r].current_value - target_output[sample]"""
-    def get_result(self, image_data):  # перед передачей сделать из image_data одномерный массив
+
+    def get_result(self, image_data):
         """
         hhh
         :param image_data:
--- nets/main.py
+++ nets/main.py
@@ -3,14 +3,20 @@ import sys  # sys нужен для передачи argv в QApplication
 from PIL import Image
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QAction
+from PyQt5.QtWidgets import QAction, QMessageBox
 import design
 
 from Perceptron import Perceptron
 
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    """
+    Класс приложения. Объединяет логику программы с интерфейсом.
+    """
     def __init__(self):
+        """
+        Конструктор. Создает экземпляр приложения. Соединяет кнопки интерфейса и действия при их нажатии.
+        """
         super().__init__()
         self.setupUi(self)
         loadAction = QAction("&Выбрать", self)
@@ -20,7 +26,7 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.perceptron = None
         self.newPerceptron.triggered.connect(self.new_perceptron)
         self.trainPerceptron_2.triggered.connect(self.train_perceptron)
-        self.dataset_perc=[]
+        self.dataset_perc = []
         self.dataset_perc.append('../dataset1/0/0.png')
         self.dataset_perc.append('../dataset1/1/1_1.png')
         self.dataset_perc.append('../dataset1/1/1_2.png')
@@ -44,6 +50,12 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.getClass.clicked.connect(self.get_class)
 
     def load_image(self):
+        """
+        Метод "Загрузить картинку". Выполняется при нажатии пользователем кнопки "Выбрать" меню "Загрузить картинку".
+        Запускает встроенное окно с проводником для выбора файла к загрузке. При выборе файла получает
+        его абсолютный путь,записывает данные о цвете пикселей и выводит загруженную картинку на экран приложения.
+        :return: Данные загруженной картинки в формате цвета пикселей. Черный - 1, белый - 0.
+        """
         dialog = QtWidgets.QFileDialog(self)
         dialog.setWindowTitle('Выберите изображение')
         filename = None
@@ -56,7 +68,7 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
             height = img.size[1]
             pix = img.load()
             img.close()
-            self.image_data=[]
+            self.image_data = []
             for w in range(width):
                 for h in range(height):
                     if pix[w, h] == (0, 0, 0, 255):  # черный
@@ -68,16 +80,61 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.label.setPixmap(pixmap)
 
     def new_perceptron(self):
+        """
+        Метод "Новый перцептрон". Выполняется при нажатии пользователем кнопки "Количество классов" подменю
+         "Перцептрон" меню "Новая сеть".Запускает встроенные окна с выбором количества S-нейронов, A-нейронов и
+         R-нейронов соответственно. Получает эти значения и на их основе создает экземпляр перцептрона. Если эти
+        параметры не были введены корректно, то создается экзепляр класса перцептрон с дефолтными параметрами.
+        :return: Созданный экземпляр перцептрона и информационное окно об успешном создании сети перцептрон.
+        """
+        s = QtWidgets.QInputDialog.getInt(self, "Введите количество S-нейронов",
+                                              "Количество S-нейронов(ширина*высота изображений):")
+        a = QtWidgets.QInputDialog.getInt(self, "Введите количество A-нейронов", "Количество A-нейронов:")
+        r = QtWidgets.QInputDialog.getInt(self, "Введите количество R-нейронов",
+                                          "Количество R-нейронов(распознаваемых классов):")
         self.perceptron = None
-        self.perceptron = Perceptron(60, 130, 10)
+        if (s[0] != 0) and (s[1] is True) and (a[0] != 0) and (a[1] is True) and (r[0] != 0) and (r[1] is True):
+            s_c = s[0]
+            a_c = a[0]
+            r_c = r[0]
+            self.perceptron = Perceptron(s_c, a_c, r_c)
+            inf = QMessageBox()
+            inf.setWindowTitle("Информация")
+            inf.setText("Новая сеть перцептрон создана. Обучите ее для дальнейшей работы с ней.")
+            inf.setIcon(QMessageBox.Information)
+            inf.setStandardButtons(QMessageBox.Ok)
+            inf.exec_()
+        else:
+            self.perceptron = Perceptron(60, 130, 10)
+            inf = QMessageBox()
+            inf.setWindowTitle("Информация")
+            inf.setText("Новая сеть перцептрон создана c значениями по умолчанию.Обучите ее для дальнейшей работы "
+                        "с ней. Для пользовательских настроек перцептрона создайте сеть заново и укажите количество"
+                        " нейронов корректно.")
+            inf.setIcon(QMessageBox.Information)
+            inf.setStandardButtons(QMessageBox.Ok)
+            inf.exec_()
         return self.perceptron
 
     def train_perceptron(self):
+        """
+        Метод "Обучить перцептрон". Выполняется при нажатии пользователем кнопки "Обучить" подменю
+         "Перцептрон" меню "Обучить сеть". Обучает перцептрон, который инициализирован в программе на основе набора
+          данных, загруженных пользователем.
+        :return: Обученный перцептрон.
+        """
         self.perceptron.train(self.dataset_perc)
         return self.perceptron
 
     def add_to_perc_dataset(self):
-        if self.default_dataset_perc == True:
+        """
+        Метод "Добавить в датасет перцептрона". Выполняется при нажатии пользователем кнопки "Добавить в датасет"
+         подменю "Перцептрон" меню "Обучить сеть". Запускает встроенное окно с проводником для выбора файла к загрузке.
+        При выборе файла получает его абсолютный путь и добавляет его в список датасета для перцептрона -
+         поля класса ExampleApp. Если этот метод не был вызван, то используется дефолтный датасет для перцептрона.
+        :return: Обновленный список набора данных для обучения перуептрона.
+        """
+        if self.default_dataset_perc:
             self.dataset_perc = []
             self.default_dataset_perc = False
         dialog = QtWidgets.QFileDialog(self)
@@ -91,8 +148,22 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                 self.dataset_perc.append(filename[i])
 
     def get_class(self):
-        result = self.perceptron.get_result(self.image_data)
-        self.classTextbox.setText(str(result))
+        """
+        Метод "Получить класс". Выполняется при нажатии пользователем кнопки "распознанный класс" главного окна
+        приложения. Получает результат работы перцептрона по распознаванию загруженной картинки.
+        :return: Распознанный класс или информационное окно с информацией о необходимости загрузить изображение,
+         если оно не было загружено.
+        """
+        if self.image_data:
+            result = self.perceptron.get_result(self.image_data)
+            self.classTextbox.setText(str(result))
+        else:
+            error = QMessageBox()
+            error.setWindowTitle("Ошибка")
+            error.setText("Для работы программы загрузите изображение")
+            error.setIcon(QMessageBox.Critical)
+            error.setStandardButtons(QMessageBox.Ok)
+            error.exec_()
 
 
 def main():
